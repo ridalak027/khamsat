8428d142a40fdd28f2749e72bc0dc26f48a7a22e
ridalak027 <ridamoh054@gmail.com>
2023-06-11 19:54:13 +0100

    khamsat

---
+++ main.js
@@ -0,0 +1,9 @@
+function openbar(){
+   document.getElementById("open").style.width = "200px";
+   document.getElementById("site").style.marginRight = "200px";
+}
+
+function closebar(){
+    document.getElementById("open").style.width = "0px";
+    document.getElementById("site").style.marginRight = "0px";
+ }
